cfb120e742801c112285937702512c42a8d3c682
Brian Balser <brian.balser@gmail.com>
2019-02-11 20:22:35 -0500

    Start

--- test/checkov_where_test.exs
+++ test/checkov_where_test.exs
@@ -2,42 +2,16 @@ defmodule Pillarcon.Checkov.WhereTest do
   use ExUnit.Case
 
   test "messing with checkov" do
-    ast = quote do
-      assert a == b
+    ast =
+      quote do
+        assert a == b
 
-      where [
-        [:a, :b],
-        [2, 2],
-        [3, 3],
-        [5, 10]
-      ]
-    end
-
-    { test_body, where } = Macro.prewalk(ast, nil, fn exp, acc ->
-      case match?({:where, _, _}, exp) do
-        true -> {nil, exp}
-        false -> {exp, acc}
+        where([
+          [:a, :b],
+          [2, 2],
+          [3, 3],
+          [5, 10]
+        ])
       end
-    end)
-
-    {:where, _, [[variables | data]]} = where
-
-    Enum.map(data, &Enum.zip(variables, &1))
-    |> Enum.map(fn binding ->
-
-      Enum.map(binding, fn {name, value} ->
-        {:=, [], [{name, [], __MODULE__}, value]}
-      end)
-
-    end)
-    |> Enum.map(fn variables ->
-      {:__block__, context, args} = test_body
-      {:__block__, context, variables ++ args}
-    end)
-   # |> Enum.map(&Macro.to_string/1)
-    |> Enum.map(fn block -> Code.eval_quoted(block,[] , __ENV__) end)
-
-
   end
-
 end
