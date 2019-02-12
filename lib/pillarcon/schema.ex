484286adcfb5da496982561c62bd068aa2dfdc8b
Brian Balser <brian.balser@gmail.com>
2019-02-11 20:03:46 -0500

    store fields

--- lib/pillarcon/schema.ex
+++ lib/pillarcon/schema.ex
@@ -1,3 +1,17 @@
 defmodule Pillarcon.Schema do
 
+  defmacro __using__(_opts) do
+    quote do
+      import Pillarcon.Schema
+      Module.register_attribute(__MODULE__, :schema_fields, accumulate: true)
+    end
+  end
+
+
+  defmacro field(name, opts) do
+    quote do
+      Module.put_attribute(__MODULE__, :schema_fields, {unquote(name), unquote(opts)})
+    end
+  end
+
 end
