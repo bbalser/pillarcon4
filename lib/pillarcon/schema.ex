54ef78c2474afb5a0ccc17a84026bf42e5afaeb4
Brian Balser <brian.balser@gmail.com>
2019-02-11 20:08:17 -0500

    default values

--- lib/pillarcon/schema.ex
+++ lib/pillarcon/schema.ex
@@ -1,5 +1,4 @@
 defmodule Pillarcon.Schema do
-
   defmacro __using__(_opts) do
     quote do
       import Pillarcon.Schema
@@ -11,18 +10,19 @@ defmodule Pillarcon.Schema do
   defmacro __before_compile__(env) do
     schema_fields = Module.get_attribute(env.module, :schema_fields)
 
-    struct_fields = Enum.map(schema_fields, fn {name, _opts} -> name end)
+    struct_fields =
+      Enum.map(schema_fields, fn {name, opts} ->
+        {name, Keyword.get(opts, :default, nil)}
+      end)
 
     quote do
       defstruct(unquote(struct_fields))
     end
   end
 
-
   defmacro field(name, opts) do
     quote do
       Module.put_attribute(__MODULE__, :schema_fields, {unquote(name), unquote(opts)})
     end
   end
-
 end
