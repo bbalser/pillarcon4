f4da7bfd0acfe658daddd5ffeaf6c398f332bdfb
Brian Balser <brian.balser@gmail.com>
2019-02-11 20:06:33 -0500

    basic struct

--- lib/pillarcon/schema.ex
+++ lib/pillarcon/schema.ex
@@ -3,7 +3,18 @@ defmodule Pillarcon.Schema do
   defmacro __using__(_opts) do
     quote do
       import Pillarcon.Schema
+      @before_compile Pillarcon.Schema
       Module.register_attribute(__MODULE__, :schema_fields, accumulate: true)
+    end
+  end
+
+  defmacro __before_compile__(env) do
+    schema_fields = Module.get_attribute(env.module, :schema_fields)
+
+    struct_fields = Enum.map(schema_fields, fn {name, _opts} -> name end)
+
+    quote do
+      defstruct(unquote(struct_fields))
     end
   end
 
